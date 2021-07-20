813d479937032adbc3d429f8e46107bf69ae0939
Caleb Anderson <buyananderson@gmail.com>
2021-07-20 14:40:53 +0200

    Congifure the htmlwebpackplug in in production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   mode: 'production',
   output: {
-    filename: '[name].[contentHash].js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/template.html',
+    }),
+  ],
 };
